8a1dca870cb17be52db450f2e3b7604fb4d5bcf7
Farid-afk <100218976+Farid-afk@users.noreply.github.com>
2022-09-08 08:13:59 +0300

    Add files via upload

---
+++ Assignment/no.1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Assignment_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter odd number btn 7 and 15");
+            int odd = Convert.ToInt16(Console.ReadLine());
+            int i;
+            int sum = 0;
+            Console.WriteLine("Odd numbers are: ");
+            for (i = 7; i < 15; i++)
+            {
+                if(i%2==1)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + 1;
+
+                }
+            }
+
+            Console.WriteLine("Sum is: " + sum);
+        }
+    }
+}
